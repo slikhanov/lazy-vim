2647465ea06127a38b98c0333c4d6599bc93dc82
Serhiy Likhanov <slikhan@gmail.com>
2023-10-13 13:38:04 -0400

    Some fixed after LazyVim update

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,8 +3,8 @@ return {
   { "folke/noice.nvim", enabled = false },
   -- disable bufferline
   { "akinsho/bufferline.nvim", enabled = false },
-  -- disable alpha-nvim
-  { "goolord/alpha-nvim", enabled = false },
+  -- disable dashboard-nvim
+  { "glepnir/dashboard-nvim", enabled = false },
   -- disable flash-nvim
   { "folke/flash.nvim", enabled = false },
 }
