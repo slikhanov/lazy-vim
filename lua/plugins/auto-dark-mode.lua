026150ae3338c17dded831dc95e609f05cce5e3b
Serhiy Likhanov <slikhan@gmail.com>
2024-10-31 20:26:20 -0400

    Switched to darker schemes

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -3,12 +3,12 @@ return {
   config = {
     update_interval = 2000,
     set_dark_mode = function()
-      vim.api.nvim_set_option("background", "dark")
-      vim.cmd("colorscheme nordfox")
+      -- vim.api.nvim_set_option("background", "dark")
+      vim.cmd("colorscheme nightfox")
     end,
     set_light_mode = function()
-      vim.api.nvim_set_option("background", "light")
-      vim.cmd("colorscheme dayfox")
+      -- vim.api.nvim_set_option("background", "light")
+      vim.cmd("colorscheme nordfox")
     end,
   },
 }
